bdae487b5ce3fcd9d3fe07f6285f93ac33870287
Marin Nikolli <sibalonat@gmail.com>
2023-10-30 02:18:23 +0100

    url for navigation

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Course
 
-class CourseSerializer(serializers.ModelSerializer):
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Course
-        fields = ('id', 'name', 'language', 'price')
+        fields = ('id', 'name', 'url', 'language', 'price')
--- courses/urls.py
+++ courses/urls.py
@@ -6,5 +6,5 @@ router = routers.DefaultRouter()
 router.register('courses', views.CourseView)
 
 urlpatterns = [
-    
+    path('', include(router.urls))    
 ]
