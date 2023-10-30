5f2425ad801f6421f292efdce60be71f3b13d7e8
Marin Nikolli <sibalonat@gmail.com>
2023-10-30 02:12:57 +0100

    url for the view

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Course
+from .serializers import CourseSerializer
 
 # Create your views here.
+
+class CourseView(viewsets.ModelViewSet) :
+    queryset = Course.objects.all()
+    serailizer_class = CourseSerializer
+
